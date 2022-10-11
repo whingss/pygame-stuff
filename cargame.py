e8b9607d48ed35b6143bfc6b05357ac9d2c383b1
Jurgen Westerhof <104225868+whingss@users.noreply.github.com>
2022-10-11 08:57:46 +0200

    added menus & other (final commit due to time)
    added menus, username text input, progress bar for loading screen
    
    will be final commit as i want to move on to some new projects

--- cargame.py
+++ cargame.py
@@ -1,11 +1,18 @@
-
 import pygame, sys
+import pygame_menu
 from pygame.locals import *
-import random
-import math
-import time
+from pygame_menu import themes
+import random, math, time
 
 pygame.init()
+
+# stuff that could be improved
+
+# TODO Make cfg file
+# TODO: add username to play menu
+# TODO: spawn car a bit further up
+# TODO: open game after level menu --> difficulty determines stuff
+# TODO: add trucks & other powerups
 
 TITLE = "Car Runner"
 
@@ -15,7 +22,7 @@ SCREENHEIGHT = 600
 FPSTICKRATE = 60
 
 SPEEDINCINTERVAL = 0.5
-MAXSPEED = 10
+MAXSPEED = 15
 PLAYERSPEED = 5
 ENEMYSPEED = 5
 SCORE = 0
@@ -33,9 +40,13 @@ game_over = font.render("Game Over", True, black)
 
 background = pygame.image.load("AnimatedStreet.png")
 
+surface = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+
+
 DISPLAYSURF = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
 DISPLAYSURF.fill(white)
 pygame.display.set_caption(TITLE)
+
 
 
 
@@ -54,7 +65,7 @@ class Player(pygame.sprite.Sprite):
                     self.rect.move_ip(-PLAYERSPEED,0)
 
         if self.rect.right < SCREENWIDTH:
-            #if not pressed_keys[K_LEFT]:
+            #if not pressed_keys[K_LEFT]:2
                 if pressed_keys[K_RIGHT]:
                     self.rect.move_ip(PLAYERSPEED,0)
 
@@ -76,6 +87,34 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.top = 0
             self.rect.center = (random.randint(30, SCREENWIDTH-30), 0)
 
+
+# ------- MENUS --------
+def set_difficulty(value, difficulty):
+    print(value)
+    print(difficulty)
+
+def start_the_game():
+    mainmenu._open(loading)
+    pygame.time.set_timer(update_loading, 30)
+
+def level_menu():
+    mainmenu._open(level)
+
+mainmenu = pygame_menu.Menu('Welcome', SCREENWIDTH, SCREENHEIGHT, theme=themes.THEME_SOLARIZED)
+mainmenu.add.text_input("Name: ", default="username", maxchar=20)
+mainmenu.add.button('Endless', start_the_game)
+mainmenu.add.button('Levels', level_menu)
+mainmenu.add.button('Quit', pygame_menu.events.EXIT)
+
+level = pygame_menu.Menu('Select a difficulty', SCREENWIDTH, SCREENHEIGHT, theme=themes.THEME_BLUE)
+level.add.selector('Difficulty: ', [("Hard", 1), ("Easy", 2)], onchange=set_difficulty)
+
+loading = pygame_menu.Menu('Loading...', SCREENWIDTH, SCREENHEIGHT, theme=themes.THEME_DARK)
+loading.add.progress_bar("Progress", progressbar_id='1', default=0, width=200)
+update_loading = pygame.USEREVENT + 2
+
+# -----------------------
+
 P1 = Player()
 E1 = Enemy()
 
@@ -90,31 +129,53 @@ INC_SPEED = pygame.USEREVENT + 1
 pygame.time.set_timer(INC_SPEED, 1000)
 
 while True:
-    for event in pygame.event.get():
-        if event.type == INC_SPEED:
-            ENEMYSPEED += SPEEDINCINTERVAL
-        if event.type == QUIT:
+    if mainmenu.is_enabled == False:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == INC_SPEED:
+                ENEMYSPEED += SPEEDINCINTERVAL
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        DISPLAYSURF.fill(white)
+        DISPLAYSURF.blit(background,(0,0))
+        scores = font_small.render(str(SCORE), True, black)
+        DISPLAYSURF.blit(scores, (10,10))
+
+        for sprite in all_sprites:
+            DISPLAYSURF.blit(sprite.image, sprite.rect)
+            sprite.move()
+        
+        if pygame.sprite.spritecollideany(P1, enemies):
+            pygame.mixer.Sound("crash.wav").play()
+            time.sleep(0.5)
+            DISPLAYSURF.fill(red)
+            DISPLAYSURF.blit(game_over, (30,250))
+            pygame.display.update()
+            for sprite in all_sprites:
+                sprite.kill()
+            time.sleep(2)
             pygame.quit()
             sys.exit()
-    
-    DISPLAYSURF.fill(white)
-    DISPLAYSURF.blit(background,(0,0))
-    scores = font_small.render(str(SCORE), True, black)
-    DISPLAYSURF.blit(scores, (10,10))
 
-    for sprite in all_sprites:
-        DISPLAYSURF.blit(sprite.image, sprite.rect)
-        sprite.move()
-    
-    if pygame.sprite.spritecollideany(P1, enemies):
-        DISPLAYSURF.fill(red)
-        DISPLAYSURF.blit(game_over, (30,250))
         pygame.display.update()
-        for sprite in all_sprites:
-            sprite.kill()
-        time.sleep(2)
-        pygame.quit()
-        sys.exit()
-    
-    pygame.display.update()
-    FPS.tick(FPSTICKRATE)
+        FPS.tick(FPSTICKRATE)
+    else:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == update_loading:
+                progress = loading.get_widget("1")
+                progress.set_value(progress.get_value() + 1)
+                if progress.get_value() == 100:
+                    pygame.time.set_timer(update_loading, 0)
+                    mainmenu.is_enabled = False
+        if mainmenu.is_enabled != False:
+            mainmenu.update(events)
+            mainmenu.draw(surface)
+            pygame.display.update()
+        
+            
